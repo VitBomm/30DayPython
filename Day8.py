ace74e10145463bf58b3b5bc321cf7ea90b36700
Vit <nnphucthinh@gmail.com>
2019-08-13 13:50:20 +0700

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -1,1 +1,17 @@
+PhoneBook = {}
+n = int(input())
+for i in range(n):
+    key,value = input().split()
+    PhoneBook[key] = value
+
+while(True):
+    try:
+        k = input()
+        if k in PhoneBook:
+            print(k+"="+PhoneBook[k])
+        else:
+            print("Not found")
+    except EOFError:
+        break
+
 
