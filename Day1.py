bb7c58ff4a6acbfd8760f6362a35409819b57047
Nguyen Phuc Thinh <thinhnguyen@hhdgroup.com>
2019-08-16 13:45:42 +0700

    Delete 14Day

--- Day1.py
+++
@@ -1,7 +0,0 @@
-i = 4
-d = 4.0
-s = 'HackerRank '
-print(i+int(input()))
-print(d+ float(input()))
-print(s + input())
-
--- Day6.py
+++
@@ -1,5 +0,0 @@
-T = int(input())
-for i in range(T):
-    a = input()
-    print("".join(a[::2])+ " "+ "".join(a[1::2]))
-
