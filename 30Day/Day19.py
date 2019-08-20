177e65799c8cde5b81095b900097b928fc7fab63
Nguyen Phuc Thinh <thinhnguyen@hhdgroup.com>
2019-08-20 09:21:24 +0700

    Added Day19

---
+++ 30Day/Day19.py
@@ -0,0 +1,18 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        total = 1
+        for i in range(2,n+1):
+            if n%i ==0:
+                total += i
+        return total
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
