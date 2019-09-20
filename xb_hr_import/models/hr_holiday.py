36d458e2ace9134d7578539fd7c371f9c8853369
Thinh Nguyen <nnphucthinh01@gmail.com>
2019-09-20 09:05:32 +0700

    [DEL]_xb_hr_import

--- xb_hr_import/models/hr_holiday.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-class HrHoliday(models.Model):
-    _inherit = 'hr.holiday'
-
-    is_imported = fields.Boolean(string="Is_imported", default=False, readonly=1)
-
-    
--- xb_hr_import/models/hr_leave.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api, fields
-
-class HrLeave(models.Model):
-    _inherit = 'hr.leave'
-    
-    is_imported = fields.Boolean(string="Is_imported", default=False, readonly=1)
-        
-    
