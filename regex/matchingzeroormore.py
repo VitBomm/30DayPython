a14407fbbc44ddb5136e04a8b2274a0def3f4ee9
System Administrator <nnphucthinh@gmail.com>
2019-09-26 09:38:28 +0700

    [FIX]_matchingzeroormore

--- regex/matchingzeroormore.py
+++ regex/matchingzeroormore.py
@@ -1,1 +1,6 @@
-a
+#https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem
+Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
