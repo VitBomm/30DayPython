524f977fb01a407bc4c80b5e84309274f0e1d649
System Administrator <nnphucthinh@gmail.com>
2019-10-08 21:02:03 +0700

    [ADD]_PositiveLookAhead

---
+++ regex/PositiveLookahead.py
@@ -0,0 +1,10 @@
+#regex_1(?=regex2)
+Regex_Pattern = r'o(?=oo)'	# Do not delete 'r'.
+
+import re
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+print(match)
+print("Number of matches :", len(match))
